4e9a4017b9d7afd54e11430bb78e5e1d9d3c7353
Elias <afloresp3@unemi.edu.ec>
2025-04-04 22:20:23 -0500

    Migraciones Hechas

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+421ca0f88502748b1b258748ca19c2602cbef4ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ba2c5b9989c739e49761e6e4d72d107a18957e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
